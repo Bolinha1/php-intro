44c64e6f85c33cd130591e87aeb785f6c1f4d8f2
eduardo.oliveira@zarpsystem.com.br <eduardo.oliveira@zarpsystem.com.br>
2018-06-05 23:34:18 -0300

    Adicionando validção de campos e exibição de mensagens de retorno

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -14,6 +17,19 @@
 <p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
 
 <form action="script.php" method="post">
+    <?php
+        $mensagemDeSucesso = isset($_SESSION['mensagem-de-sucesso']) ? $_SESSION['mensagem-de-sucesso'] : '';
+        if(!empty($mensagemDeSucesso))
+        {
+            echo $mensagemDeSucesso;
+        }
+
+        $mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : '';
+        if(!empty($mensagemDeErro))
+        {
+            echo $mensagemDeErro;
+        }
+    ?>
     <p>Seu nome: <input type="text" name="nome" /></p>
     <p>Sua idade: <input type="text" name="idade" /></p>
     <p><input type="submit" value="Enviar dados do competidor"/></p>
--- script.php
+++ script.php
@@ -5,6 +5,9 @@
  * Date: 03/06/18
  * Time: 16:56
  */
+
+session_start();
+
 $catetorias = [];
 $categorias[] = 'infantil';
 $categorias[] = 'adolescente';
@@ -16,23 +19,26 @@ $idade = $_POST['idade'];
 
 if(empty($nome))
 {
-    echo 'O nome não pode ser vazio';
+    $_SESSION['mensagem-de-erro'] = 'O nome não pode ser vazio, por favor preencha-o novamente';
+    header('location: index.php');
     return;
 }
-
-if(strlen($nome) < 3)
+else if(strlen($nome) < 3)
 {
-    echo 'O nome deve conter mais de 3 caracteres';
+    $_SESSION['mensagem-de-erro'] = 'O nome não pode conter menos de 3 caracteres';
+    header('location: index.php');
     return;
 }
-if(strlen($nome) > 40)
+else if(strlen($nome) > 40)
 {
-    echo "O nome é muito extenso";
+    $_SESSION['mensagem-de-erro'] = 'O nome não pode conter mais de 40 caracteres';
+    header('location: index.php');
     return;
 }
-if(!is_numeric($idade))
+else if(!is_numeric($idade))
 {
-    echo "Informe um número para idade";
+    $_SESSION['mensagem-de-erro'] = 'Informe um número para idade';
+    header('location: index.php');
     return;
 }
 
@@ -42,7 +48,11 @@ if($idade >= 6 && $idade <= 12)
     for($i = 0; $i <= count($categorias); $i++)
     {
         if($categorias[$i] == 'infantil')
-            echo "O nadador ".$nome. " compete na categoria " .$categorias[$i];
+        {
+            $_SESSION['mensagem-de-sucesso'] =  "O nadador ".$nome. " compete na categoria " .$categorias[$i];
+            header("location: index.php");
+            return;
+        }
     }
 }
 else if($idade >= 13 && $idade <= 18)
@@ -50,7 +60,11 @@ else if($idade >= 13 && $idade <= 18)
     for($i = 0; $i <= count($categorias); $i++)
     {
         if($categorias[$i] == 'adolescente')
-            echo "O nadador ".$nome. " compete na categoria adolescente";
+        {
+            $_SESSION['mensagem-de-sucesso'] =  "O nadador ".$nome. " compete na categoria " .$categorias[$i];
+            header("location: index.php");
+            return;
+        }
     }
 }
 else
@@ -58,7 +72,11 @@ else
     for($i = 0; $i <= count($categorias); $i++)
     {
         if($categorias[$i] == 'adulto')
-            echo "O nadador ".$nome. " compete na categoria adulto";
+        {
+            $_SESSION['mensagem-de-sucesso'] =  "O nadador ".$nome. " compete na categoria " .$categorias[$i];
+            header("location: index.php");
+            return;
+        }
     }
 }
 
